98b13930b5503e32f43c7ff307b03e83effe9b7b
scps940707 <scps940707@gmail.com>
2023-06-06 16:03:30 +0800

    Initial commit

---
+++ src/main/java/com/scps/werd/WerdApplication.java
@@ -0,0 +1,13 @@
+package com.scps.werd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WerdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WerdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scps/werd/WerdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scps.werd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WerdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
