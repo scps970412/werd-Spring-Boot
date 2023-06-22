c4fdec337fc573ebe28f0108c6d3afd81ed44518
unknown <scps970412@gmail.com>
2023-06-23 03:02:41 +0800

    [DEV] Add Supplier Model

---
+++ src/main/java/com/scps/werd/model/supplier.java
@@ -0,0 +1,26 @@
+package com.scps.werd.model;
+
+import java.security.Timestamp;
+
+public class supplier {
+    private Timestamp createTime;
+    private String creator;
+    private Timestamp updateTime;
+    private String updater;
+    private String remark;
+    private boolean isDeleted;
+    private String id;
+    private String supplier;
+    private String unifiedBusinessNo;
+    private String email;
+    private String address1;
+    private String address2;
+    private String head;
+    private String headPhone1;
+    private String headPhone2;
+    private String contactPerson;
+    private String contactPersonPhone1;
+    private String contactPersonPhone2;
+    private String otherContact1;
+    private String otherContact2;
+}
